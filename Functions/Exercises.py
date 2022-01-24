806c9e67bdac2a5ace5a6920bd21ae75d660e43f
Sebastian Gracia <sebasgraciavalderrama@users.noreply.github.com>
2022-01-24 16:44:56 -0500

    Last set of exercises and a couple of attempts to solve summer of 69

--- Functions/Exercises.py
+++ Functions/Exercises.py
@@ -1,14 +1,8 @@
 def lesser_of_two_evens(a,b):
    if (a%2==0) and (b%2==0):
-        if (a<b):
-            return a
-        else:
-            return b
+       return min(a,b)
    else:
-       if (a>b):
-            return a
-       else:
-            return b
+       return max(a,b)
 
 def animal_crackes(text):
     split_text = text.split()
@@ -30,6 +24,91 @@ def old_macdonald(name):
     fourthLetterCapitalized = name[3:].capitalize()
     return (firstLetterCapitalized+fourthLetterCapitalized)
 
+def master_yoda(text):
+    split_text = text.split()
+    first_word = split_text[0]
+    last_word = split_text[-1]
+    split_text[0] = last_word
+    split_text[-1] = first_word
+    return ' '.join(split_text)
+
+def almost_there(n):
+    if (n>= 90 and n<=110) or (n>= 190 and n<=210):
+        return True
+    return False
+
+def has_33(nums):
+    for i, num in enumerate(nums):
+        if num[i] == 3 and num [i+1]==3:
+            return True
+    return False
+
+def paper_doll(text):
+    letters = list(text)
+    new_string = []
+    for letter in letters:
+        new_string.append(letter)
+        new_string.append(letter)
+        new_string.append(letter)
+    return ''.join(new_string)
+
+def blackjack(a,b,c):
+    total = a+b+c
+    if total <= 21:
+        return total
+    elif total >= 21 and 11 in (a,b,c):
+        return total-10
+    else:
+        return 'BUST'
+
+def summer_69(arr):
+    total = 0
+    flag = True
+    for number in arr:
+        if number == 6 or number == 9:
+            flag = nine_or_six(number)
+        if flag:
+            total += number
+    return total
+
+def nine_or_six(num):
+    if num == 6 :
+        return False
+    if num == 9:
+        return True
+
+def summer_69_actual_solution(arr):
+    total = 0
+    add = True
+    for num in arr:
+        while add:
+            if num != 6:
+                total += num
+                break
+            else:
+                add = False
+        while not add:
+            if num != 9:
+                break
+            else:
+                add = True
+                break
+    return total
+
+def summer_69_powered(arr):
+    s = set(arr)
+    if 6 in s and 9 in s:
+        where_is_six = arr.index(6)
+        where_is_nine = arr.index(9)
+        how_many_times_to_pop = 0
+        for i in range(where_is_six, where_is_nine+1):
+            how_many_times_to_pop = how_many_times_to_pop + 1
+        for index in range (how_many_times_to_pop):
+            arr.pop()
+        return (sum(arr))
+    else:
+        return 0
+
 string1 = 'Levelheaded Llama'
 string2 = 'Crazy Kangaroo'
 
@@ -45,6 +124,28 @@ print(makes_twenty(2,3))
 
 print(old_macdonald('macdonald'))
 
+print(master_yoda('I am home'))
+print(master_yoda('We are ready'))
 
+print(almost_there(90))
+print(almost_there(104))
+print(almost_there(150))
+print(almost_there(209))
 
+print(paper_doll('Hello'))
+print(paper_doll('Mississippi'))
 
+print(blackjack(5,6,7))
+print(blackjack(9,9,9))
+print(blackjack(9,9,11))
+
+print(summer_69([1, 3, 5]))
+print(summer_69([4, 5, 6, 7, 8, 9]))
+print(summer_69([2, 1, 6, 9, 11]))
+
+print('##############################')
+
+#print(summer_69_powered([1, 3, 5]))
+print(summer_69_powered([1, 6, 3, 5]))
+#print(summer_69_powered([4, 5, 6, 7, 8, 9]))
+#print(summer_69_powered([2, 1, 6, 9, 11]))
