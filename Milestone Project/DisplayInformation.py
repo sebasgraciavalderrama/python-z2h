097919de7492d3a3a9c30e6a59dfa1c749584c04
Sebastian Gracia <sebasgraciavalderrama@users.noreply.github.com>
2022-01-26 08:26:33 -0500

    Milestone project review

---
+++ Milestone Project/DisplayInformation.py
@@ -0,0 +1,17 @@
+print('Hello')
+print([1,2,3])
+
+def display(row1, row2, row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+
+row1 = [' ', ' ', ' ']
+row2 = [' ', ' ', ' ']
+row3 = [' ', ' ', ' ']
+display(row1,row2,row3)
+
+
+display([1,2,3],[1,2,3],[1,2,3])
+
---
+++ Milestone Project/UserInput.py
@@ -0,0 +1,8 @@
+result = input('Please enter a value: ')
+result_int = int(result)
+result_float = float(result)
+print(type(result_int))
+print(type(result_float))
+
+new_result = int(input('Please enter a new value: '))
+print(type(new_result))
