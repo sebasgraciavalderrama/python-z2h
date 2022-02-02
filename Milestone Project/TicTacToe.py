bc05882547b678db69075db95ec2f68c03ab0987
Sebastian Gracia <sebasgraciavalderrama@users.noreply.github.com>
2022-02-02 09:55:21 -0500

    tictactoe - play again logic, fixes to win function

--- Milestone Project/TicTacToe.py
+++ Milestone Project/TicTacToe.py
@@ -2,12 +2,14 @@ tictactoe_board = [['1','2','3'],
                    ['4','5','6'],
                    ['7','8','9']]
 
+
 def tie(player=''): # Check if at any given point in time the result of the match is a tie, returns True if tie
+
     print('Match has resulted with a tie!')
     return True
 
 def win(tictactoe_board, player=''): # Check if any player won after inserting a 'X' or 'O' in the board, returns True if any player has won
-    if tictactoe_board[0][0] == tictactoe_board[0][1] == tictactoe_board[2][0] == player:
+    if tictactoe_board[0][0] == tictactoe_board[1][0] == tictactoe_board[2][0] == player:
         # win_position_vertical_1 = {(0,0),(1,0),(2,0)}
         print(f'Player {player} has won!')
         return True
@@ -23,8 +25,8 @@ def win(tictactoe_board, player=''): # Check if any player won after inserting a
         # win_position_horizontal_1 = {(0,0),(0,1),(0,2)}
         print(f'Player {player} has won!')
         return True
-    if tictactoe_board[1][0] == tictactoe_board[1][1] == tictactoe_board[0][2] == player:
-        # win_position_horizontal_2 = {(1,0),(1,1),(0,2)}
+    if tictactoe_board[1][0] == tictactoe_board[1][1] == tictactoe_board[1][2] == player:
+        # win_position_horizontal_2 = {(1,0),(1,1),(1,2)}
         print(f'Player {player} has won!')
         return True
     if tictactoe_board[2][0] == tictactoe_board[2][1] == tictactoe_board[2][2] == player:
@@ -40,7 +42,6 @@ def win(tictactoe_board, player=''): # Check if any player won after inserting a
         print(f'Player {player} has won!')
         return True
     return False
-
 
 def position_available(*args): # Given a position on the board, check if that position is available to be inserted or if its already taken
     return any (args in x for x in tictactoe_board)
@@ -76,6 +77,19 @@ def print_board(tictactoe_board): # Prints board after any update (instert, win,
         print(" ".join(map(str, item)))
     print("#---------------------------------------#")
 
+def want_to_play_again():
+    choice = 'WRONG'
+    while choice == 'WRONG':
+        decision = input('Want to play again? (Y/N): ').upper()
+        if decision == 'Y':
+            return False
+        elif decision == 'N':
+            return True
+
+def clean_board(tictactoe_board):
+    tictactoe_board = [['1','2','3'],
+                       ['4','5','6'],
+                       ['7','8','9']]
 #-----------------------------------------MAIN SECTION - BEGIN-----------------------------------------#
 
 # Initialization of variables
@@ -94,33 +108,46 @@ else:
 while not anybody_won: # While loop to play until any player won or tied match
     valid_input = False
     if not player_turn: # If statement to control players turn
+        player_symbol = 'X'
         while not valid_input: # While loop to get users 1)input 2)insertion 3)validation of win or tie
             print('Your turn X...')
             print_board(tictactoe_board)
-            player_symbol = 'X'
             player_choice = input('Where would you like to place your symbol?: ')
             valid = insert_player_choice(tictactoe_board, player_choice, player_symbol)
             if valid:
                 # Insertion was successful, next player can play
                 valid_input = True
                 player_turn = True
-        '''
-        TODO: Win function
-        anybody_won = win(player_symbol)
-        '''
+                anybody_won = win(tictactoe_board, player_symbol)
     elif player_turn:
+        player_symbol = 'O'
         while not valid_input:
             print('Your turn O...')
             print_board(tictactoe_board)
-            player_symbol = 'O'
             player_choice = input('Where would you like to place your symbol?: ')
             valid = insert_player_choice(tictactoe_board, player_choice, player_symbol)
             if valid:
                 # Insertion was successful, next player can play
                 valid_input = True
                 player_turn = False
-        '''
-        TODO: Win function
-        anybody_won = win(player_symbol)
-        '''
+                anybody_won = win(tictactoe_board, player_symbol)
+
+    if anybody_won:
+        # Would you like to play again?
+        anybody_won =  want_to_play_again()
+        if not anybody_won:
+            player_turn = False
+            anybody_won = False
+            valid_input = False
+            clean_board(tictactoe_board)
+            print("Welcome again to Sebastian's poorly-optimized-TicTacToe game!!!")
+            # If statement to define which player 'X' or 'Y' gets to play first.
+            decision = who_goes_first()
+            if decision == 'X':
+                player_turn = False
+            else:
+                player_turn = True
+
+print("Thank you for playing!, See you soon...")
+
 #-----------------------------------------MAIN SECTION - END-----------------------------------------#
