89eb9a2d6b5d3a84ba94f00aa8b8afb7d40addf9
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-02-09 16:16:11 -0500

    pytest testing

---
+++ Milestone Project pt2/BlackJack/Tests/Math.py
@@ -0,0 +1,14 @@
+def calc_addition(a, b):
+    return a+b
+
+def cal_multiply(a, b):
+    return a*b
+
+def cal_division(a,b):
+    return a/b
+
+def cal_substraction(a,b):
+    return a-b
+
+def cal_power(a,b):
+    return a**b
---
+++ Milestone Project pt2/BlackJack/Tests/test_math.py
@@ -0,0 +1,21 @@
+import Math
+
+def test_calc_addition():
+    result = Math.calc_addition(4,2)
+    assert result == 6
+
+def test_cal_multiply():
+    result = Math.cal_multiply(2,4)
+    assert result == 8
+
+def test_cal_division():
+    result = Math.cal_division(4,2)
+    assert result == 2
+
+def test_cal_substraction():
+    result = Math.cal_substraction(4,2)
+    assert result == 2
+
+def test_cal_power():
+    result = Math.cal_power(2,2)
+    assert result == 4
