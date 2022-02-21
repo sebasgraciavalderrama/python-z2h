5b7c8ab11597ac1e5909ce60fabf4cfdc9ad9413
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-02-21 11:29:13 -0500

    Puzzle challenge solution and some refactoring

--- Modules/Puzzle/PuzzleSolution.py
+++ Modules/Puzzle/PuzzleSolution.py
@@ -1,26 +1,25 @@
-import shutil, os
+import shutil, os, re
 
 shutil.unpack_archive('unzip_me_for_instructions.zip', 'unzip_me_for_instructions_folder', 'zip')
 
-unzipme_location = '/Users/sgracia/Documents/GitHub/Z2H-Python/python-z2h/Modules/Puzzle/unzip_me_for_instructions_folder'
 extracted_content = '/Users/sgracia/Documents/GitHub/Z2H-Python/python-z2h/Modules/Puzzle/unzip_me_for_instructions_folder/extracted_content'
+phone_number = []
 
-my_phone_numbers = []
-
-'''for folder, sub_folders, files in os.walk(unzipme_location):
-    print(f"Currently looking at {folder}")
-    print('\n')
-    print("The subfolders are: ")
-    for sub_fold in sub_folders:
-        print(f"\tSubfolder: {sub_folders}")
-    print('\n')
-    print('The files are: ')
-    for f in files:
-        print(f"\t File: {f}")
-    print('\n')'''
-
+counter = 1
 for folder, sub_folders, files in os.walk(extracted_content):
-    for sub_fold in sub_folders:
-        print(sub_fold)
-        for f in files:
-            print(f)
+    for special_file in files:
+        file_path = os.path.join(folder, special_file)
+        with open(file_path, 'r+') as read_file:
+            #print('reading file: ' + str(counter))
+            for line in read_file:
+                #print(line)
+                phone = re.search(r'\d{3}-\d{3}-\d{4}', line)
+                if phone is not None:
+                    phone_number.append(phone.group())
+                    phone_number.append(counter)
+            counter += 1
+
+print(f'Total number of files: {counter}')
+print('\n')
+print(f'Phone number found in file number: {phone_number[1]}')
+print(phone_number[0])
