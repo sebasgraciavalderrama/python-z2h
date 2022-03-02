bafc9e1d32db5318d4a16adad68f68ac65eeed4b
Sebastian Gracia <sebastiangracia123@gmail.com>
2022-03-02 11:49:52 -0500

    Puzzle solution pt 2

--- CSV&PDF/Puzzle/Puzzle.py
+++ CSV&PDF/Puzzle/Puzzle.py
@@ -1,7 +1,7 @@
 # Task 1 - Use Python to extract the Google Drive link from the .csv file. (Hint: Its along the diagonal from top left to bottom right).
-import csv, re
+import csv, re, numpy, PyPDF2
 
-# Open the file
+# Open the file - THIS DOES
 data = open('find_the_link.csv', encoding='utf-8')
 # Call csv.reader
 csv_data = csv.reader(data, delimiter=',')
@@ -14,6 +14,8 @@ for item in data_lines:
         char = re.search(r'\D', character)
         # If the regex does not return a valid find (any character different from a number)
         # we move on until we find the next valid find.
+        # KEEP IN MIND THIS DOES NOT ALLOW NUMBERS
+        # TO BE IN THE URL, THEREFOR ONLY WORKS FOR URL'S WITH NO NUMBERS.
         if char is not None:
             # We add the character to the list
             url_by_char.append(character)
@@ -21,5 +23,25 @@ for item in data_lines:
 url = ''.join(url_by_char)
 print(url)
 
+# Using diagonal function from NumPy
+url_by_char_numpy = []
+url_numpy = ''.join(numpy.diagonal(data_lines))
+print(url_numpy)
 
 # Task 2 - Task Two: Download the PDF from the Google Drive link (we already downloaded it for you just in case you can’t download from Google Drive) and find the phone number that is in the document.
+f = open('Find_the_Phone_Number.pdf', 'rb')
+pdf_text = []
+pdf_reader = PyPDF2.PdfFileReader(f)
+phone_number = ''
+for num in range(pdf_reader.numPages):
+    page = pdf_reader.getPage(num)
+    pdf_text.append(page.extractText())
+
+print(f'Number of pages from the extracted PDF: {len(pdf_text)}' )
+#print("Text from PDF: ", *pdf_text, sep='\n')
+#print(pdf_text)
+for text in pdf_text:
+    phone = re.search(r'\d{3}.\d{3}.\d{4}', text)
+    if phone is not None:
+        phone_number = phone.group().replace('.', ' ')
+print(f'The phone number found in the PDF is: {phone_number}')
